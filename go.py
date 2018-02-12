4bafb93e5cdd7aeae5beb5e8e7f1b1da0c0b083d
Josh Hoak <jrhoak@gmail.com>
2018-02-12 11:52:30 -0800

    Dummy commit to test out testing.

--- go.py
+++ go.py
@@ -20,6 +20,9 @@ A PlayerMove is a (Color, Move) tuple
 
 (0, 0) is considered to be the upper left corner of the board, and (18, 0) is the lower left.
 '''
+
+# move zigs
+
 from collections import namedtuple
 import copy
 import itertools
